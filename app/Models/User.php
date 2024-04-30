cd31430f93decea461e99e6fb7843978ae647a19
ladyrokhaya <ladyrokhaya@gmail.com>
2024-04-30 00:32:16 +0000

    configuration jwt okay

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -43,5 +43,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function getJWTCustomClaims()
+    {
+        return [] ;
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
     }
 }
