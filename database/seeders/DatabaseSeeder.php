c80df82ca2b3258e6a532b12e8465744078cc45d
Làye DIOP <adiop600@gmail.com>
2024-05-07 08:38:57 +0000

    ...

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Seeder;
 use Database\Seeders\RoleSeeder;
 use Database\Seeders\UserSeeder;
 
+use App\Models\FormationGrade;
+use App\Models\SubDomain;
+use App\Models\Formation;
+use App\Models\School;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -19,9 +24,45 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+
+        $school = School::create([
+            'school_name' => 'Acadec School',
+            'user_id' => '1',
+            'mobile' => '771724497',
+            'phone' => '338359898',
+            'email' => 'contact@acadec.net',
+            'address' => 'Yoff Virage',
+            'website' => 'https://acadec.net',
+        ]);
+         $subdomain = SubDomain::create([
+            'name' => 'Informatique',
+            'description_domaine' => 'Etude sur la logique et de la technologie'
+         ]);
+
+        $grades = FormationGrade::insert([
+            [
+                "formation_grade" => "BTS",
+            ],[
+                "formation_grade" => "License",
+            ],[
+                "formation_grade" => "Master",
+            ],[
+                "formation_grade" => "Doctorat",
+            ],
+        ]);
+
+        Formation::create([
+            'formation_name' => 'Genie Informatique',
+            'formation_type' => 'jour',
+            'class_format' => 'prensentiel',
+            'accreditation' => 'Anaqsup',
+            'formation_duration' => '3',
+            'study_level_required' => 'Baccalauret',
+            'registration_payment' => '150000',
+            'monthly_payment' => '50000',
+            'school_id' => $school->id,
+            'formation_grade_id' => 1,
+            'sub_domain_id' => $subdomain->id,
         ]);
     }
 }
